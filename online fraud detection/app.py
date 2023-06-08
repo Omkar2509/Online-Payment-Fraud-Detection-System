f605e59f9ccc8f44ec048cee4dc40a206c9b7355
Omkar2509 <91217750+Omkar2509@users.noreply.github.com>
2023-06-08 16:04:54 +0530

    Add files via upload

---
+++ online fraud detection/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)   
+
+model=pickle.load(open('fraud_detection_model.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[float(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict(final)
+    output=prediction[0]
+
+    if output== "Fraud":
+        return render_template('result.html',pred='Fraud')
+    else:
+        return render_template('result.html', pred='Not Fraud')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
